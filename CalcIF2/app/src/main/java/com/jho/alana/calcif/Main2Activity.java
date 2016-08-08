10a47c18928baf1225b58c8787a15c8c27c4c9e5
JhoanesFreitas <jhoanesbarbosa@gmail.com>
2016-08-08 01:47:01 -0300

    pog intermediário necessário

--- CalcIF2/app/src/main/java/com/jho/alana/calcif/Main2Activity.java
+++ CalcIF2/app/src/main/java/com/jho/alana/calcif/Main2Activity.java
@@ -1,7 +1,5 @@
 package com.jho.alana.calcif;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -19,7 +17,11 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.jho.alana.fragments.CalcFragmentl;
+import com.jho.alana.fragments.Pager;
 import com.jho.alana.myStack.MyStack;
+
+import java.util.zip.Inflater;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -52,29 +54,29 @@ public class Main2Activity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        //Fragment d = mSectionsPagerAdapter.getItem(1);
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
 
         ViewGroup viewGroup = (ViewGroup) toolbar.getParent();
         LayoutInflater inflater = getLayoutInflater();
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        View view = inflater.inflate(R.layout.content_main, viewGroup, true);
-        tvCalc = (TextView) view.findViewById(R.id.tvCalc);
+        //mViewPager.getCurrentItem();
+
+        //View view = inflater.inflate(R.layout.content_main, mViewPager, true);
+
+        //tvCalc = new TextView(this);
+
+        //mViewPager.findViewById(R.id.tvCalc);
+        //mViewPager.findViewById()
+        //tvCalc = (TextView) view.findViewById(R.id.tvCalc);
+
     }
 
     @Override
@@ -99,45 +101,6 @@ public class Main2Activity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section expression for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * expression.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.content_main, container, false);
-
-
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -155,7 +118,7 @@ public class Main2Activity extends AppCompatActivity {
         //public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return PlaceholderFragment.newInstance(position + 1);
+                    return CalcFragmentl.newInstance();  //PlaceholderFragment.newInstance(position + 1);
                 case 1:
                     return Pager.newInstance();
             }
@@ -171,10 +134,29 @@ public class Main2Activity extends AppCompatActivity {
 
     //Método que mostra os caracteres digitados na view
     public void onClick(View view) {
-        expression += ((TextView)view).getText().toString();
-        Log.d("string", ((TextView)view).getText().toString());
-        //tvCalc.setText(((TextView) view).getText().toString());
-        tvCalc.setText(expression);
+
+        switch(view.getId()){
+            
+            case R.id.btnBack:
+                backspace(view);
+            case R.id.btnDel:
+                clearAll(view);
+            default:
+                expression += ((TextView)view).getText().toString();
+                Log.d("string", ((TextView)view).getText().toString());
+                //tvCalc.setText(((TextView) view).getText().toString());
+                tvCalc.setText(expression);
+                Log.d("string", tvCalc.getText().toString());
+        }
+
+    }
+
+    /**
+     * Gambiarra pura para poder pegar a instância do TextView. Para isso,
+     * deve-se dar um clique nesse textView antes de qualquer outra coisa...
+     * */
+    public void getScreen(View view){
+        tvCalc = (TextView) view;
     }
 
     //Método para apagar um caractere
@@ -191,7 +173,6 @@ public class Main2Activity extends AppCompatActivity {
     //Método para apagar todos os caracteres da View
     public void clearAll(View view) {
         //String cls = ((TextView)view).getText().toString();
-        tvCalc = ((TextView) findViewById(R.id.tvCalc));
         tvCalc.setText("");
     }
 
